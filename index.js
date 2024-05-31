354c17bd114edf1adad029398cc27de46098792e
Martina Lazcano <martinalazcano18@gmail.com>
2024-05-31 10:50:58 -0300

    lower case

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ do{
     
 
     //Sentencia switch para que el juego reaccione dependiendo de la jugada del usuario
-    switch (partidaUser){
+    switch (partidaUser.toLowerCase){
         case "piedra":
             if (partidaOrdenador === 0){
                 alert("Yo elegi piedra tambien!. Empate :)");
